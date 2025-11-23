f95f125d95ca2a9a93ae552d833b8c78d40da007
MShpiz <reg@panfilova.com>
2025-11-23 18:52:47 +0300

    added: Quest list element, Short Quest list info, icons (stars, download)

--- app/src/main/java/com/eduplay/moblie/repository/TaskRepository.kt
+++ app/src/main/java/com/eduplay/moblie/repository/TaskRepository.kt
@@ -1,9 +1,21 @@
 package com.eduplay.moblie.repository
 
+import com.eduplay.moblie.repository.responseTypes.AuthResponse
 import com.eduplay.moblie.repository.webrepository.WebRepository
 import javax.inject.Inject
 
 class TaskRepository @Inject constructor(
     private val webRepository: WebRepository
 ): Repository {
+    override suspend fun login(): AuthResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun logout() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun register() {
+        TODO("Not yet implemented")
+    }
 }
