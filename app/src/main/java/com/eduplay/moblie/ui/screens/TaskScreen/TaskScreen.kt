21d18ffeabeae29bbbdecba5b8a1425f63ea90c9
Panfilova Maria <reg@panfilova.com>
2026-01-06 15:49:45 +0300

    added: general ui for Task Screen

--- app/src/main/java/com/eduplay/moblie/ui/screens/TaskScreen/TaskScreen.kt
+++ app/src/main/java/com/eduplay/moblie/ui/screens/TaskScreen/TaskScreen.kt
@@ -1,19 +1,205 @@
 package com.eduplay.moblie.ui.screens.TaskScreen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.eduplay.moblie.R
+import com.eduplay.moblie.models.TaskType
+import kotlin.math.max
+
+//TODO("optional scroll bar для column")
 
 @Composable
-fun TaskScreen() {
-    // top bar
+fun TaskScreen(innerPaddingValues: PaddingValues) {
+    val taskType = TaskType.SINGLE_CHOICE
+    BoxWithConstraints {
+        val height = maxHeight
+        Column(
+            modifier = Modifier
+                .padding(
+                    top = 0.dp,
+                    bottom = innerPaddingValues.calculateBottomPadding(),
+                    start = innerPaddingValues.calculateStartPadding(LayoutDirection.Ltr),
+                    end = innerPaddingValues.calculateEndPadding(LayoutDirection.Ltr)
+                )
+                .fillMaxSize()
+        ) {
+            // top bar
+            TaskTopBar()
 
-    // header
 
-    //task
+            // header
+            TaskHeader(height)
 
-    //next btn
+            //task
+
+            Box(modifier = Modifier.weight(2f)) {
+                when (taskType) {
+                    TaskType.INFO -> Box {} //TODO("info task type")
+                    TaskType.SINGLE_CHOICE -> SingleChoiceTask(height)
+                    TaskType.MULTIPLE_CHOICE -> Box {} //TODO("multiple choice task type")
+                    TaskType.TEXT -> Box {} //TODO("text type")
+                    TaskType.QR -> Box {} //TODO("qr task text type")
+                }
+            }
+
+            //next btn
+            SubmitBtn(taskType)
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun TaskTopBar() {
-
+    TopAppBar(
+        title = {},
+        navigationIcon = {
+            IconButton(
+                onClick = { TODO("task screen btn back") }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.go_back)
+                )
+            }
+        }
+    )
 }
+
+@Composable
+private fun TaskHeader(maxHeight: Dp) {
+    val title = "very Loooooooooooong and hard question"
+    val description =
+        """
+          Description aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        """.trimIndent()
+
+    var showTimer by remember { mutableStateOf(true) }
+    var currentProgress by remember { mutableFloatStateOf(0.5f) }
+    val timeLeft by remember { mutableIntStateOf(30) }
+
+    // Timer
+    Column(modifier = Modifier.fillMaxWidth()) {
+        if (showTimer) {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.9f)
+            ) {
+                LinearProgressIndicator(
+                    progress = { currentProgress },
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .fillMaxWidth()
+                        .height(35.dp)
+                        .padding(vertical = 10.dp)
+
+                )
+                Text(
+                    text = "${timeLeft / 60}:${timeLeft % 60}",
+                    style = typography.titleLarge,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+            }
+        }
+
+        // task title an description
+        Column(
+            Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(0.9f)
+                .heightIn(50.dp, max(50, maxHeight.value.toInt()/3).dp)
+                .padding(vertical = 15.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            // title
+            Text(
+                text = title,
+                style = typography.headlineSmall
+                    .copy(fontWeight = FontWeight.Bold, textAlign = TextAlign.Center),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            //description
+            Text(
+                text = description,
+                style = typography.bodyMedium,
+                //.copy(fontSize = 20.sp),
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(top = 15.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun SubmitBtn(taskType: TaskType) {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .padding(vertical = 15.dp)
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth(0.9f)
+        ) {
+            Text(
+                text = if (taskType == TaskType.INFO) stringResource(R.string.proceed)
+                else stringResource(R.string.answer),
+                style = typography.headlineSmall,
+
+                )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TaskPreview() {
+    TaskScreen(PaddingValues())
+}
+
