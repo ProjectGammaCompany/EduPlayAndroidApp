a5acd154bb13f943f5d9da3f8600ed9a1d9b5de2
MShpiz <reg@panfilova.com>
2026-01-11 16:54:49 +0300

    added: Event results screen

---
+++ app/src/main/java/com/eduplay/moblie/ui/screens/EventResultScreen.kt
@@ -0,0 +1,133 @@
+package com.eduplay.moblie.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.eduplay.moblie.R
+
+@Composable
+fun EventResultScreen(innerPaddingValues: PaddingValues) {
+    val points = 33
+    Column(
+        modifier = Modifier
+            .padding(
+                top = 0.dp,
+                bottom = innerPaddingValues.calculateBottomPadding(),
+                start = innerPaddingValues.calculateStartPadding(LayoutDirection.Ltr),
+                end = innerPaddingValues.calculateEndPadding(LayoutDirection.Ltr)
+            )
+            .fillMaxSize()
+    ) {
+        ResultTopBar()
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+
+
+            Text(
+                text = stringResource(R.string.congratulation),
+                style = typography.headlineMedium,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth()
+            )
+            FlowRow(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(0.9f)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = stringResource(R.string.you_got) + ":",
+                    style = typography.titleLarge,
+                )
+                Text(
+                    text = " $points ",
+                    style = typography.titleLarge
+                )
+                Text(
+                    text = stringResource(R.string.points),
+                    style = typography.titleLarge,
+                )
+            }
+            Image(
+                painter = painterResource(id = R.drawable.correct_answer),
+                contentDescription = stringResource(R.string.correct),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .sizeIn(
+                        minHeight = 200.dp,
+                        minWidth = 200.dp,
+                        maxWidth = 500.dp,
+                        maxHeight = 500.dp
+                    )
+
+            )
+        }
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(0.9f)
+        ) {
+            Text(stringResource(R.string.ok))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ResultTopBar() {
+    TopAppBar(
+        title = {},
+        navigationIcon = {
+            IconButton(
+                onClick = { TODO("result screen btn back") }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.go_back)
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun ResulPreview() {
+    EventResultScreen(PaddingValues())
+}
