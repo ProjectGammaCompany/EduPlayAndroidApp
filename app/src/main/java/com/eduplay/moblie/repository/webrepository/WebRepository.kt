a292756907978023d8b40de781f407439fe135e5
Panfilova Maria <reg@panfilova.com>
2026-04-24 17:27:44 +0300

    added logging

--- app/src/main/java/com/eduplay/moblie/repository/webrepository/WebRepository.kt
+++ app/src/main/java/com/eduplay/moblie/repository/webrepository/WebRepository.kt
@@ -491,11 +491,13 @@ class WebRepository @Inject constructor(
 
     suspend fun postAnswerBatch(answerBatch: AnswerBatch): Boolean {
         val response = api.postAnswerBatch(answerBatch)
+        Log.d("post_answer_batch", response.code().toString() + response.raw())
         return response.isSuccessful
     }
 
     suspend fun getDownloadedEventsStatus(events: List<String>): List<UserEventStatus>? {
         val response = api.getUserEventsStatuses(EventIdList(events))
+        Log.d("get_downloaded_events_status", response.code().toString() + response.raw())
         val body = response.body()
         if (response.isSuccessful && body != null) {
             return body.events
